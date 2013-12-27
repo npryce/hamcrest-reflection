2a311470c21e3390ccadf455ff38bef4b5fc1f02
headcrashing@googlemail.com <headcrashing@googlemail.com@a24b22bc-622d-11de-85c0-7b9747e48d0e>
2013-12-27 15:23:35 +0000

    Fixed bug #1 "listsMatchingNestedClassesInReverseInheritanceOrder failing on Java 7" (https://code.google.com/p/hamcrest-reflection/issues/detail?id=1)
    Cause: False assumption in test design. JRE does not guarantee that static members and non-static members are returned in any particular order by java.lang.Class#getDeclaredClasses(). In fact, it seems that the order was changed between between past JRE releases.
    
    Solution: Wrote private Hamcrest matcher which checks two groups of items to exist in a given order in a (possibly larger) list. Hence, the false assumption (static comes after non-static) is replaced by a relaxed assumption (child declarations come before parent declarations). The latter actually is implemented by Hamcrest Reflection, hence is the sole valid issue to be actually checked in this test.

--- src/test/java/com/natpryce/hamcrest/reflection/tests/ReflectomaticTest.java
+++ src/test/java/com/natpryce/hamcrest/reflection/tests/ReflectomaticTest.java
@@ -5,6 +5,11 @@ import static com.natpryce.hamcrest.reflection.ModifierMatcher.withModifiers;
 import static com.natpryce.hamcrest.reflection.Reflectomatic.constructorsOf;
 import static com.natpryce.hamcrest.reflection.Reflectomatic.fieldsOf;
 import static com.natpryce.hamcrest.reflection.Reflectomatic.methodsOf;
+import static com.natpryce.hamcrest.reflection.tests.ReflectomaticTest.ItemsBeforeMatcher.ItemsFollowMatcherBuilder.hasItems;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 import static java.lang.reflect.Modifier.PUBLIC;
 import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.anything;
@@ -12,7 +17,10 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Ignore;
+import java.util.List;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
 
 import com.natpryce.hamcrest.reflection.Reflectomatic;
@@ -56,14 +64,11 @@ public class ReflectomaticTest {
     }
 
     @Test
-	@Ignore("False assumption, test might fail sporadically. See issue 1 (https://code.google.com/p/hamcrest-reflection/issues/detail?id=1).")
     public void listsMatchingNestedClassesInReverseInheritanceOrder() throws Exception {
         assertThat(
-                Reflectomatic.classesIn(DerivedClass.class, anything()), 
-                equalTo(asList(DerivedClass.InnnerClassInDerivedClass.class, 
-                               DerivedClass.StaticInnerclassInDerivedClass.class,
-                               BaseClass.InnnerClassInBaseClass.class,
-                               BaseClass.StaticInnerclassInBaseClass.class)));
+                Reflectomatic.classesIn(DerivedClass.class, anything()),
+                hasItems(DerivedClass.InnnerClassInDerivedClass.class, DerivedClass.StaticInnerclassInDerivedClass.class)
+                .beforeItems(BaseClass.InnnerClassInBaseClass.class, BaseClass.StaticInnerclassInBaseClass.class));
     }
 
     @Test
@@ -85,4 +90,89 @@ public class ReflectomaticTest {
     	Reflectomatic.copyFieldsFromTo(from, to);
     	assertThat(to.fieldA, equalTo(from.fieldA));
     }
+    
+	/**
+	 * A Matcher checking whether some items occur in a {@code List} before some other items. The list may contain more items than the checked ones.
+	 * 
+	 * @author Markus KARG (markus@headcrashing.eu)
+	 * 
+	 * @param <T>
+	 *            Type of list items.
+	 */
+	static final class ItemsBeforeMatcher<T> extends TypeSafeMatcher<List<T>> {
+		private final List<T> leadingItems;
+		private final List<T> trailingItems;
+
+		private ItemsBeforeMatcher(final List<T> leadingItems, final List<T> trailingItems) {
+			this.leadingItems = leadingItems;
+			this.trailingItems = trailingItems;
+		}
+
+		@Override
+		public final boolean matchesSafely(final List<T> list) {
+			return this.lastLeadingItemIsBeforeFirstTrailingItemIn(list);
+		}
+
+		private final boolean lastLeadingItemIsBeforeFirstTrailingItemIn(final List<T> list) {
+			return this.lastLeadingIndexIn(list) < this.firstTrailingIndexIn(list);
+		}
+
+		private final int lastLeadingIndexIn(final List<T> list) {
+			return lastIndex(list, this.leadingItems);
+		}
+
+		private final int firstTrailingIndexIn(final List<T> list) {
+			return firstIndex(list, this.trailingItems);
+		}
+
+		private static final <T> int firstIndex(final List<T> list, final List<T> checkedItems) {
+			int i = MAX_VALUE;
+			for (final T item : checkedItems)
+				i = min(i, list.indexOf(item));
+			return i;
+		}
+
+		private static final <T> int lastIndex(final List<T> list, final List<T> checkedItems) {
+			int i = MIN_VALUE;
+			for (final T item : checkedItems)
+				i = max(i, list.lastIndexOf(item));
+			return i;
+		}
+
+		public final void describeTo(final Description description) {
+			description.appendValueList("has items [", ", ", "]", this.leadingItems).appendValueList(" before items [", ", ", "]", this.trailingItems);
+		}
+
+		static final class ItemsFollowMatcherBuilder<T> {
+			@SafeVarargs
+			public static final <T> ItemsFollowMatcherBuilder<T> hasItems(final T... leadingItems) {
+				return new ItemsFollowMatcherBuilder<T>(leadingItems);
+			}
+
+			@SafeVarargs
+			public final ItemsBeforeMatcher<T> beforeItems(final T... trailingItems) {
+				return new ItemsBeforeMatcher<T>(asList(this.leadingItems), asList(trailingItems));
+			}
+
+			private final T[] leadingItems;
+
+			@SafeVarargs
+			private ItemsFollowMatcherBuilder(final T... leadingItems) {
+				this.leadingItems = leadingItems;
+			}
+		}
+	}
+
+	@Test
+	public final void shouldAssertThatTrailingItemsFollowLeadingItems() {
+		// given
+		final List<Character> someList = asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H');
+
+		// when
+		final Character[] ofFirstGroup = new Character[] { 'B', 'D' };
+		final Character[] ofSecondGroup = new Character[] { 'E', 'G' };
+
+		// then
+		assertThat(someList, hasItems(ofFirstGroup).beforeItems(ofSecondGroup));
+	}
 }
