9261b020310a06be57e40591e64de0936cc0157a
headcrashing@googlemail.com <headcrashing@googlemail.com@a24b22bc-622d-11de-85c0-7b9747e48d0e>
2013-12-28 08:57:23 +0000

    Using Autoformat

--- src/main/java/com/natpryce/hamcrest/reflection/HasAnnotationMatcher.java
+++ src/main/java/com/natpryce/hamcrest/reflection/HasAnnotationMatcher.java
@@ -10,41 +10,39 @@ import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 
 public class HasAnnotationMatcher<T extends Annotation> extends TypeSafeDiagnosingMatcher<AnnotatedElement> {
-    private final Class<T> annotationType;
-    private final Matcher<? super T> annotationMatcher;
-    
-    public HasAnnotationMatcher(Class<T> annotationType, Matcher<? super T> annotationMatcher) {
-        this.annotationType = annotationType;
-        this.annotationMatcher = annotationMatcher;
-    }
+	private final Class<T> annotationType;
+	private final Matcher<? super T> annotationMatcher;
 
-    @Override
-    protected boolean matchesSafely(AnnotatedElement item, Description mismatchDescription) {
-        T annotation = item.getAnnotation(annotationType);
-        if (annotation == null) {
-            mismatchDescription
-                .appendText("does not have annotation ")
-                .appendText(annotationType.getName());
-            return false;
-        }
-        
-        if (!annotationMatcher.matches(annotation)) {
-            annotationMatcher.describeMismatch(annotation, mismatchDescription);
-            return false;
-        }
-        
-        return true;
-    }
+	public HasAnnotationMatcher(Class<T> annotationType, Matcher<? super T> annotationMatcher) {
+		this.annotationType = annotationType;
+		this.annotationMatcher = annotationMatcher;
+	}
 
-    public void describeTo(Description description) {
-        
-    }
+	@Override
+	protected boolean matchesSafely(AnnotatedElement item, Description mismatchDescription) {
+		T annotation = item.getAnnotation(annotationType);
+		if (annotation == null) {
+			mismatchDescription.appendText("does not have annotation ").appendText(annotationType.getName());
+			return false;
+		}
 
-    public static Matcher<AnnotatedElement> hasAnnotation(Class<? extends Annotation> annotationType) {
-        return hasAnnotation(annotationType, anything(""));
-    }
-    
-    public static <T extends Annotation> Matcher<AnnotatedElement> hasAnnotation(Class<T> annotationType, Matcher<? super T> annotationMatcher) {
-        return new HasAnnotationMatcher<T>(annotationType, annotationMatcher);
-    }
+		if (!annotationMatcher.matches(annotation)) {
+			annotationMatcher.describeMismatch(annotation, mismatchDescription);
+			return false;
+		}
+
+		return true;
+	}
+
+	public void describeTo(Description description) {
+
+	}
+
+	public static Matcher<AnnotatedElement> hasAnnotation(Class<? extends Annotation> annotationType) {
+		return hasAnnotation(annotationType, anything(""));
+	}
+
+	public static <T extends Annotation> Matcher<AnnotatedElement> hasAnnotation(Class<T> annotationType, Matcher<? super T> annotationMatcher) {
+		return new HasAnnotationMatcher<T>(annotationType, annotationMatcher);
+	}
 }
--- src/main/java/com/natpryce/hamcrest/reflection/Reflectomatic.java
+++ src/main/java/com/natpryce/hamcrest/reflection/Reflectomatic.java
@@ -14,66 +14,66 @@ import java.util.List;
 import org.hamcrest.Matcher;
 
 public class Reflectomatic {
-    public interface FeatureType<T> {
-        T[] featuresOf(Class<?> fromClass);
-    }
-    
-    public static FeatureType<Field> FIELDS = new FeatureType<Field>() {
-        public Field[] featuresOf(Class<?> fromClass) {
-            return fromClass.getDeclaredFields();
-        }
-    };
+	public interface FeatureType<T> {
+		T[] featuresOf(Class<?> fromClass);
+	}
 
-    public static FeatureType<Method> METHODS = new FeatureType<Method>() {
-        public Method[] featuresOf(Class<?> fromClass) {
-            return fromClass.getDeclaredMethods();
-        }
-    };
-    
-    public static FeatureType<Class<?>> CLASSES = new FeatureType<Class<?>>() {
-        public Class<?>[] featuresOf(Class<?> fromClass) {
-            return fromClass.getDeclaredClasses();
-        }
-    };
-    
-    private static <T> List<T> all(Class<?> type, FeatureType<T> featureType, Matcher<? super T> criteria) {
-        List<T> matchingFeatures = new ArrayList<T>();
-        
-        for (Class<?> t = type; t != null; t = t.getSuperclass()) {
-            collectMatchingFeatures(featureType.featuresOf(t), criteria, matchingFeatures);
-        }
-        
-        return matchingFeatures;
-    }
-    
-    private static <T> void collectMatchingFeatures(T[] features, Matcher<? super T> criteria, List<T> collection) {
-        for (T feature : features) {
-            if (criteria.matches(feature)) {
-                collection.add(feature);
-            }
-        }
-    }
-    
-    public static List<Field> fieldsOf(Class<?> type, Matcher<? super Field> criteria) {
-        return all(type, FIELDS, criteria);
-    }
-    
-    public static List<Method> methodsOf(Class<?> type, Matcher<? super Method> criteria) {
-        return all(type, METHODS, criteria);
-    }
-    
-    public static List<Constructor<?>> constructorsOf(Class<?> type, Matcher<? super Constructor<?>> criteria) {
-        List<Constructor<?>> matchingConstructors = new ArrayList<Constructor<?>>();
-        collectMatchingFeatures(type.getDeclaredConstructors(), criteria, matchingConstructors);
-        return matchingConstructors;
-    }
-    
-    public static List<Class<?>> classesIn(Class<?> type, Matcher<? super Class<?>> criteria) {
-        return all(type, CLASSES, criteria);
-    }
+	public static FeatureType<Field> FIELDS = new FeatureType<Field>() {
+		public Field[] featuresOf(Class<?> fromClass) {
+			return fromClass.getDeclaredFields();
+		}
+	};
+
+	public static FeatureType<Method> METHODS = new FeatureType<Method>() {
+		public Method[] featuresOf(Class<?> fromClass) {
+			return fromClass.getDeclaredMethods();
+		}
+	};
+
+	public static FeatureType<Class<?>> CLASSES = new FeatureType<Class<?>>() {
+		public Class<?>[] featuresOf(Class<?> fromClass) {
+			return fromClass.getDeclaredClasses();
+		}
+	};
+
+	private static <T> List<T> all(Class<?> type, FeatureType<T> featureType, Matcher<? super T> criteria) {
+		List<T> matchingFeatures = new ArrayList<T>();
+
+		for (Class<?> t = type; t != null; t = t.getSuperclass()) {
+			collectMatchingFeatures(featureType.featuresOf(t), criteria, matchingFeatures);
+		}
+
+		return matchingFeatures;
+	}
+
+	private static <T> void collectMatchingFeatures(T[] features, Matcher<? super T> criteria, List<T> collection) {
+		for (T feature : features) {
+			if (criteria.matches(feature)) {
+				collection.add(feature);
+			}
+		}
+	}
+
+	public static List<Field> fieldsOf(Class<?> type, Matcher<? super Field> criteria) {
+		return all(type, FIELDS, criteria);
+	}
+
+	public static List<Method> methodsOf(Class<?> type, Matcher<? super Method> criteria) {
+		return all(type, METHODS, criteria);
+	}
+
+	public static List<Constructor<?>> constructorsOf(Class<?> type, Matcher<? super Constructor<?>> criteria) {
+		List<Constructor<?>> matchingConstructors = new ArrayList<Constructor<?>>();
+		collectMatchingFeatures(type.getDeclaredConstructors(), criteria, matchingConstructors);
+		return matchingConstructors;
+	}
+
+	public static List<Class<?>> classesIn(Class<?> type, Matcher<? super Class<?>> criteria) {
+		return all(type, CLASSES, criteria);
+	}
 
 	public static void copyFieldsFromTo(Object from, Object to) {
-		for (Field field : fieldsOf(to.getClass(), not(withModifiers(FINAL|STATIC)))) {
+		for (Field field : fieldsOf(to.getClass(), not(withModifiers(FINAL | STATIC)))) {
 			try {
 				field.setAccessible(true);
 				field.set(to, field.get(from));
